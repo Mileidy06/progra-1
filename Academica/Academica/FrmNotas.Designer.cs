f23700155fc5df37c7880a602131e61fd464f328
Mileidy Gonzalez <Mileidy@DESKTOP-DIB0FPO>
2025-10-28 20:39:35 -0600

    .

---
+++ Academica/Academica/FrmNotas.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Academica
+{
+    partial class FrmNotas
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Label lblidNota;
+            System.Windows.Forms.Label lblMateria;
+            System.Windows.Forms.Label lblPeriodo;
+            System.Windows.Forms.Label lblFecha;
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.dnotasDataGridView = new System.Windows.Forms.DataGridView();
+            this.idNotaLabel1 = new System.Windows.Forms.Label();
+            this.cboMateria = new System.Windows.Forms.ComboBox();
+            this.cboPeriodo = new System.Windows.Forms.ComboBox();
+            this.fechaDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.alumno = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            lblidNota = new System.Windows.Forms.Label();
+            lblMateria = new System.Windows.Forms.Label();
+            lblPeriodo = new System.Windows.Forms.Label();
+            lblFecha = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dnotasDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblidNota
+            // 
+            lblidNota.AutoSize = true;
+            lblidNota.Location = new System.Drawing.Point(115, 45);
+            lblidNota.Name = "lblidNota";
+            lblidNota.Size = new System.Drawing.Size(45, 13);
+            lblidNota.TabIndex = 11;
+            lblidNota.Text = "Id Nota:";
+            // 
+            // lblMateria
+            // 
+            lblMateria.AutoSize = true;
+            lblMateria.Location = new System.Drawing.Point(115, 74);
+            lblMateria.Name = "lblMateria";
+            lblMateria.Size = new System.Drawing.Size(45, 13);
+            lblMateria.TabIndex = 13;
+            lblMateria.Text = "Materia:";
+            // 
+            // lblPeriodo
+            // 
+            lblPeriodo.AutoSize = true;
+            lblPeriodo.Location = new System.Drawing.Point(115, 101);
+            lblPeriodo.Name = "lblPeriodo";
+            lblPeriodo.Size = new System.Drawing.Size(46, 13);
+            lblPeriodo.TabIndex = 15;
+            lblPeriodo.Text = "Periodo:";
+            // 
+            // lblFecha
+            // 
+            lblFecha.AutoSize = true;
+            lblFecha.Location = new System.Drawing.Point(115, 129);
+            lblFecha.Name = "lblFecha";
+            lblFecha.Size = new System.Drawing.Size(40, 13);
+            lblFecha.TabIndex = 17;
+            lblFecha.Text = "Fecha:";
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(450, 44);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(75, 23);
+            this.btnGuardar.TabIndex = 20;
+            this.btnGuardar.Text = "GUARDAR";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // dnotasDataGridView
+            // 
+            this.dnotasDataGridView.AllowUserToAddRows = false;
+            this.dnotasDataGridView.AllowUserToDeleteRows = false;
+            this.dnotasDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dnotasDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.alumno});
+            this.dnotasDataGridView.Location = new System.Drawing.Point(52, 172);
+            this.dnotasDataGridView.Name = "dnotasDataGridView";
+            this.dnotasDataGridView.Size = new System.Drawing.Size(643, 256);
+            this.dnotasDataGridView.TabIndex = 19;
+            // 
+            // idNotaLabel1
+            // 
+            this.idNotaLabel1.Location = new System.Drawing.Point(178, 45);
+            this.idNotaLabel1.Name = "idNotaLabel1";
+            this.idNotaLabel1.Size = new System.Drawing.Size(200, 23);
+            this.idNotaLabel1.TabIndex = 12;
+            this.idNotaLabel1.Text = "label1";
+            // 
+            // cboMateria
+            // 
+            this.cboMateria.DisplayMember = "Materia";
+            this.cboMateria.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboMateria.FormattingEnabled = true;
+            this.cboMateria.Location = new System.Drawing.Point(178, 71);
+            this.cboMateria.Name = "cboMateria";
+            this.cboMateria.Size = new System.Drawing.Size(200, 21);
+            this.cboMateria.TabIndex = 14;
+            this.cboMateria.ValueMember = "idMateria";
+            this.cboMateria.SelectedIndexChanged += new System.EventHandler(this.cboMateria_SelectedIndexChanged);
+            // 
+            // cboPeriodo
+            // 
+            this.cboPeriodo.DisplayMember = "periodo";
+            this.cboPeriodo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboPeriodo.FormattingEnabled = true;
+            this.cboPeriodo.Location = new System.Drawing.Point(178, 98);
+            this.cboPeriodo.Name = "cboPeriodo";
+            this.cboPeriodo.Size = new System.Drawing.Size(200, 21);
+            this.cboPeriodo.TabIndex = 16;
+            this.cboPeriodo.ValueMember = "IdPeriodo";
+            this.cboPeriodo.SelectedIndexChanged += new System.EventHandler(this.cboPeriodo_SelectedIndexChanged);
+            // 
+            // fechaDateTimePicker
+            // 
+            this.fechaDateTimePicker.Location = new System.Drawing.Point(178, 125);
+            this.fechaDateTimePicker.Name = "fechaDateTimePicker";
+            this.fechaDateTimePicker.Size = new System.Drawing.Size(200, 20);
+            this.fechaDateTimePicker.TabIndex = 18;
+            // 
+            // alumno
+            // 
+            this.alumno.DataPropertyName = "IdDetalle";
+            this.alumno.HeaderText = "ALUMNO";
+            this.alumno.Name = "alumno";
+            this.alumno.ReadOnly = true;
+            this.alumno.Width = 200;
+            // 
+            // FrmNotas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.dnotasDataGridView);
+            this.Controls.Add(lblidNota);
+            this.Controls.Add(this.idNotaLabel1);
+            this.Controls.Add(lblMateria);
+            this.Controls.Add(this.cboMateria);
+            this.Controls.Add(lblPeriodo);
+            this.Controls.Add(this.cboPeriodo);
+            this.Controls.Add(lblFecha);
+            this.Controls.Add(this.fechaDateTimePicker);
+            this.Name = "FrmNotas";
+            this.Text = "FrmNotas";
+      
+            ((System.ComponentModel.ISupportInitialize)(this.dnotasDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.DataGridView dnotasDataGridView;
+        private System.Windows.Forms.Label idNotaLabel1;
+        private System.Windows.Forms.ComboBox cboMateria;
+        private System.Windows.Forms.ComboBox cboPeriodo;
+        private System.Windows.Forms.DateTimePicker fechaDateTimePicker;
+        private System.Windows.Forms.DataGridViewTextBoxColumn alumno;
+    }
+}
